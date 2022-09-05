024a5517264b8c18174a11bf160fd1a9693ef512
Chad <chad@cysense.io>
2022-09-05 12:51:11 +0800

    Fix comments

--- azuread/table_azuread_device.go
+++ azuread/table_azuread_device.go
@@ -30,6 +30,12 @@ func tableAzureAdDevice(_ context.Context) *plugin.Table {
 			KeyColumns: plugin.KeyColumnSlice{
 				// Key fields
 				{Name: "display_name", Require: plugin.Optional},
+				{Name: "account_enabled", Require: plugin.Optional, Operators: []string{"<>", "="}},
+				{Name: "operating_system", Require: plugin.Optional},
+				{Name: "operating_system_version", Require: plugin.Optional},
+				{Name: "profile_type", Require: plugin.Optional},
+				{Name: "trust_type", Require: plugin.Optional},
+				{Name: "filter", Require: plugin.Optional},
 			},
 		},
 
@@ -38,10 +44,13 @@ func tableAzureAdDevice(_ context.Context) *plugin.Table {
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "The unique identifier for the device. Inherited from directoryObject.", Transform: transform.FromMethod("GetId")},
 			{Name: "display_name", Type: proto.ColumnType_STRING, Description: "The name displayed for the device.", Transform: transform.FromMethod("GetDisplayName")},
 
+			{Name: "filter", Type: proto.ColumnType_STRING, Transform: transform.FromQual("filter"), Description: "Odata query to search for resources."},
+
 			{Name: "account_enabled", Type: proto.ColumnType_BOOL, Description: "True if the account is enabled; otherwise, false.", Transform: transform.FromMethod("GetAccountEnabled")},
 			{Name: "approximate_last_sign_in_date_time", Type: proto.ColumnType_TIMESTAMP, Description: "The timestamp type represents date and time information using ISO 8601 format and is always in UTC time.", Transform: transform.FromMethod("GetApproximateLastSignInDateTime")},
 			{Name: "device_id", Type: proto.ColumnType_STRING, Description: "Unique identifier set by Azure Device Registration Service at the time of registration.", Transform: transform.FromMethod("GetDeviceId")},
 			{Name: "extension_attributes", Type: proto.ColumnType_JSON, Description: "Contains extension attributes 1-15 for the device. The individual extension attributes are not selectable. These properties are mastered in cloud and can be set during creation or update of a device object in Azure AD.", Transform: transform.FromMethod("GetExtensions")},
+			{Name: "filter", Type: proto.ColumnType_STRING, Transform: transform.FromQual("filter"), Description: "Odata query to search for resources."},
 			{Name: "is_compliant", Type: proto.ColumnType_BOOL, Description: "True if the device is compliant; otherwise, false.", Transform: transform.FromMethod("GetIsCompliant")},
 			{Name: "is_managed", Type: proto.ColumnType_BOOL, Description: "True if the device is managed; otherwise, false.", Transform: transform.FromMethod("GetIsManaged")},
 			{Name: "mdm_app_id", Type: proto.ColumnType_STRING, Description: "Application identifier used to register device into MDM.", Transform: transform.FromMethod("GetMdmAppId")},
@@ -90,8 +99,8 @@ func listDevices(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 	input.Expand = expandColumns
 
 	var queryFilter string
-	filter := buildQueryFilter(equalQuals)
-	filter = append(filter, buildBoolNEFilter(quals)...)
+	filter := buildDeviceQueryFilter(equalQuals)
+	filter = append(filter, buildDeviceBoolNEFilter(quals)...)
 
 	if equalQuals["filter"] != nil {
 		queryFilter = equalQuals["filter"].GetStringValue()
@@ -139,7 +148,7 @@ func listDevices(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 	}
 
 	for _, device := range result.GetValue() {
-		d.StreamLeafListItem(ctx, &ADDeviceInfo{device})
+		d.StreamListItem(ctx, &ADDeviceInfo{device})
 
 		if d.QueryStatus.RowsRemaining(ctx) == 0 {
 			return nil, nil
@@ -218,4 +227,55 @@ func getDevice(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) 
 	}
 
 	return &ADDeviceInfo{device}, nil
+}
+
+func buildDeviceQueryFilter(equalQuals plugin.KeyColumnEqualsQualMap) []string {
+	filters := []string{}
+
+	filterQuals := map[string]string{
+		"display_name":             "string",
+		"id":                       "string",
+		"operating_system":         "string",
+		"operating_system_version": "string",
+		"profile_type":             "string",
+		"trust_type":               "string",
+		"account_enabled":          "bool",
+	}
+
+	for qual, qualType := range filterQuals {
+		switch qualType {
+		case "string":
+			if equalQuals[qual] != nil {
+				filters = append(filters, fmt.Sprintf("%s eq '%s'", strcase.ToCamel(qual), equalQuals[qual].GetStringValue()))
+			}
+		case "bool":
+			if equalQuals[qual] != nil {
+				filters = append(filters, fmt.Sprintf("%s eq %t", strcase.ToCamel(qual), equalQuals[qual].GetBoolValue()))
+			}
+		}
+	}
+
+	return filters
+}
+
+func buildDeviceBoolNEFilter(quals plugin.KeyColumnQualMap) []string {
+	filters := []string{}
+
+	filterQuals := []string{
+		"account_enabled",
+	}
+
+	for _, qual := range filterQuals {
+		if quals[qual] != nil {
+			for _, q := range quals[qual].Quals {
+				value := q.Value.GetBoolValue()
+				if q.Operator == "<>" {
+					filters = append(filters, fmt.Sprintf("%s eq %t", strcase.ToCamel(qual), !value))
+					break
+				}
+			}
+		}
+	}
+
+	return filters
 }
