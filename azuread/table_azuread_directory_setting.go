fddb4a271b9e6fe489cf9bd37919e91091d3d21c
Subhajit Mondal <subhajit@turbot.com>
2022-10-07 13:07:55 +0530

    Update tables to use steampipe-plugin-sdk v4.1.7

--- azuread/table_azuread_directory_setting.go
+++ azuread/table_azuread_directory_setting.go
@@ -5,9 +5,9 @@ import (
 
 	msgraphcore "github.com/microsoftgraph/msgraph-sdk-go-core"
 	"github.com/microsoftgraph/msgraph-sdk-go/models"
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 //// TABLE DEFINITION
